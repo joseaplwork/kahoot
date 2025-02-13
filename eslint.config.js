22936086dfe00969aa224a77b2b6f762edd5732f
joseaplwork <joseaplwork@gmail.com>
2025-02-13 01:30:02 +0100

    chore: add react-router and unused imports ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'unused-imports/no-unused-imports': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
