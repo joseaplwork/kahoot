6532405119dec7816ec26ff8765e3afb67864115
joseaplwork <joseaplwork@gmail.com>
2025-02-17 00:02:14 +0100

    refactor: optimize audio component to download audio files on user interaction

--- src/shared/components/Audio/Audio.tsx
+++ src/shared/components/Audio/Audio.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './Audio.css'
 
@@ -7,55 +7,32 @@ interface Props {
 }
 
 export function Audio({ url }: Props) {
-  const audioRef = useRef<HTMLAudioElement>(null)
+  const audioRef = useRef<HTMLAudioElement | null>(null)
   const [playing, setPlaying] = useState(false)
-  const [canPlay, setCanPlay] = useState<boolean>(!!url)
+  const [error, setError] = useState(false)
 
-  const handleAudioEnd = () => {
-    setPlaying(false)
-  }
-
-  useEffect(() => {
-    const audio = audioRef.current
-
-    if (audio) {
-      audio.addEventListener('ended', handleAudioEnd)
-
-      return () => {
-        audio.removeEventListener('ended', handleAudioEnd)
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    const audio = document.createElement('audio')
-    const format = url.split('.').pop()?.toLowerCase()
-
-    if (format) {
-      const canPlay = audio.canPlayType(`audio/${format}`)
-
-      setCanPlay(canPlay !== '')
-    }
-  }, [url])
-
-  const handlePlay = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handlePlay = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
 
-    if (audioRef.current) {
+    try {
+      if (!audioRef.current) {
+        audioRef.current = new window.Audio(url)
+        audioRef.current.addEventListener('ended', () => setPlaying(false))
+      }
+
+      await audioRef.current.play()
       setPlaying(true)
-      audioRef.current.play()
+    } catch {
+      setError(true)
     }
   }
 
   return (
-    <button className="audio-button" disabled={!canPlay} onClick={handlePlay}>
+    <button className="audio-button" disabled={error} onClick={handlePlay}>
       {playing ? (
         <div className="audio-button__playing"></div>
       ) : (
         <div className="audio-button__play-button"></div>
-      )}
-      {canPlay && (
-        <audio ref={audioRef} src={url} onError={() => setCanPlay(false)} />
       )}
     </button>
   )
